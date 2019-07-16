7829d0dbbefe28f758d0b016887ace45a7fe56fc
deshi9012 <ilia.bojadzhiev@gmail.com>
2019-07-16 16:23:05 +0300

    ssss

--- Modules/Leads/Resources/views/table_view.blade.php
+++ Modules/Leads/Resources/views/table_view.blade.php
@@ -1,21 +1,4 @@
-<style>
-	/*li {*/
-		/*list-style-type: none;*/
-		/*position: relative;    !* It is required for setting position to absolute in the next rule. *!*/
-	/*}*/
 
-	/*li::before {*/
-		/*content: '\2022';      !* Unicode for • character *!*/
-		/*position: absolute;*/
-		/*bottom: -0.4em;*/
-		/*left: -0.8em;          !* Adjust this value so that it appears where you want. *!*/
-		/*font-size: 3.3em;      !* Adjust this value so that it appears what size you want. *!*/
-	/*}*/
-	/*td{*/
-		/*padding-top: 1px!important;*/
-		/*padding-bottom: 1px!important;*/
-	/*}*/
-</style>
 <div class="col-lg-12">
 	<section class="panel panel-default">
 
